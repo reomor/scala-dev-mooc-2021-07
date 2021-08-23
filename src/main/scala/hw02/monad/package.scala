987a2bc9b69356be6cc8475b80077aeab540ca82
Reomor <reomor@gmail.com>
2021-08-24 00:04:40 +0300

    hw02 changed tasks

---
+++ src/main/scala/hw02/monad/package.scala
@@ -0,0 +1,43 @@
+package hw02
+
+package object monad {
+
+    /**
+     * Реализуйте методы map / flatMap / withFilter чтобы работал код и законы монад соблюдались
+     * HINT: для проверки на пустой элемент можно использовать eq
+     */
+
+    trait Wrap[+A] {
+
+        def get: A
+
+        def pure[R](x: R): Wrap[R] = NonEmptyWrap[R](x)
+
+        def flatMap[R](f: A => Wrap[R]): Wrap[R] = this match {
+            case EmptyWrap => EmptyWrap
+            case NonEmptyWrap(v) => f(v)
+        }
+
+        // HINT: map можно реализовать через pure и flatMap
+        def map[R](f: A => R): Wrap[R] = flatMap(v => pure(f(v)))
+
+        def withFilter(f: A => Boolean): Wrap[A] = this match {
+            case EmptyWrap => EmptyWrap
+            case NonEmptyWrap(v) => if (f(v)) NonEmptyWrap(v) else EmptyWrap
+        }
+
+    }
+
+    object Wrap {
+        def empty[R]: Wrap[R] = EmptyWrap
+    }
+
+    case class NonEmptyWrap[A](result: A) extends Wrap[A] {
+        override def get: A = result
+    } // pure
+
+    case object EmptyWrap extends Wrap[Nothing] {
+        override def get: Nothing = throw new NoSuchElementException("Wrap.get")
+    } // bottom, null element
+
+}
