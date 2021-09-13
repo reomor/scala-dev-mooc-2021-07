33c5fbddc31f4d2473e6db2ad4bd7ebeb25e841f
avoronets <avoronets@naumen.ru>
2021-09-13 22:10:20 +0300

    webinar 13
    (cherry picked from commit 5bf4777a0faec1c217f60c20854b707a8bd284ac)

--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -6,11 +6,34 @@ import zio.ExitCode
 import zio.ZEnv
 import zio.URIO
 import module3.zioOperators
+import zio.Cause
+import zio.console._
+import zio.IO
+import zio.duration._
+import scala.language.postfixOps
+
+
 
 object App {
 
   def main(args: Array[String]): Unit = {
-    zio.Runtime.default.unsafeRun(ZIO.effect(println("Hello")))
+
+    sealed trait NotificationError
+    case object NotificationByEmailFailed extends NotificationError
+    case object NotificationBySMSFailed extends NotificationError
+
+    val z1 = ZIO.fail(NotificationByEmailFailed)
+    val z2 = ZIO.fail(NotificationBySMSFailed)
+
+    val z3 = z1.zipPar(z2)
+
+    val z4 = z3.tapCause{
+      case Cause.Both(left, right) => 
+        putStrLn(left.failureOption.toString()) *> putStrLn(right.failureOption.toString())
+    }.orElse(putStrLn("app failed"))
+  
+
+    zio.Runtime.default.unsafeRun(z4)
   }
 }
 
